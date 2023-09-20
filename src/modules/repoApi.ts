37313fdde023d46b05f1b0de88e410c403da68c7
INIRU <iniru@kakao.com>
2023-09-20 17:09:37 +0900

    repo api token bug fix

--- src/modules/repoApi.ts
+++ src/modules/repoApi.ts
@@ -3,12 +3,11 @@ import axios, { AxiosResponse } from 'axios';
 let url = 'https://api.github.com/users/INIRU/repos';
 
 function repoApi(): Promise<AxiosResponse<any>> {
-  console.log(process.env.REACT_APP_GIT_TOKEN);
   return axios({
     url: url,
     headers: {
       Accept: 'application/vnd.github+json',
-      Authorization: `Bearer ${process.env.REACT_APP_GIT_TOKEN}`,
+      Authorization: `${process.env.REACT_APP_GIT_TOKEN}`,
       'X-GitHub-Api-Version': '2022-11-28',
     },
   });
