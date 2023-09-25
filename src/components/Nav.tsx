46b3f6f3aa8b2a42042960d14798a2eaabeea61c
INIRU <iniru@kakao.com>
2023-09-25 09:47:03 +0900

    Revert "nav herf del"
    This reverts commit e8a0d726f5756afc2abd94139e01f4ea3f059072.
    
    	수정함:        src/App.tsx

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -27,6 +27,7 @@ function NavBar(props: { up: boolean; view: string }): JSX.Element {
       >
         <Navbar.Brand
           className="d-flex align-items-center gap-1"
+          href="#home"
           onClick={() => {
             navigate('/');
           }}
