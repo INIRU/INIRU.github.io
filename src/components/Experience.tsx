393c662176f8adb7c15a573ac6bec3c1a4fc87f0
INIRU <iniru@kakao.com>
2023-09-30 19:51:20 +0900

    mobile flexible

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -2,6 +2,7 @@ import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import MarkdownPreview from '@uiw/react-markdown-preview';
 import { useEffect, useState } from 'react';
+import { Container } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 
 function Experience(): JSX.Element {
@@ -31,9 +32,13 @@ function Experience(): JSX.Element {
   }
 
   useEffect(() => {
-    setFade('fade-out');
+    let timer = setTimeout(() => {
+      setFade('fade-out');
+    }, 100);
 
-    return () => {};
+    return () => {
+      clearTimeout(timer);
+    };
   }, [fade]);
 
   useEffect(() => {
@@ -45,38 +50,40 @@ function Experience(): JSX.Element {
       <h2 className="code-html text-center pt-3">
         &lt;<span className="code-html-tag">Experience</span>&gt;
       </h2>
-      <div className={`mt-4 w-100 d-flex gap-3 ${fade}`}>
-        <FontAwesomeIcon
-          className="ps-2 experi-page align-self-center"
-          icon={faAngleLeft}
-          onClick={() => {
-            PageBack();
-          }}
-        ></FontAwesomeIcon>
-        <div className="w-100">
-          <h2 className="experi-title">
-            <span className="experi-year">
-              <MarkdownPreview
-                source={`\`${experi[page].year}\``}
-                className="experi-year-md"
-              />
-            </span>
-            {experi[page].title}
-          </h2>
-          <MarkdownPreview
-            source={experi[page].desc}
-            className="experience mt-3"
-          />
+      <Container>
+        <div className={`mt-5 w-100 d-flex gap-3 ${fade}`}>
+          <FontAwesomeIcon
+            className="experi-page align-self-center"
+            icon={faAngleLeft}
+            onClick={() => {
+              PageBack();
+            }}
+          ></FontAwesomeIcon>
+          <div className="w-100">
+            <h2 className="experi-title">
+              <span className="experi-year">
+                <MarkdownPreview
+                  source={`\`${experi[page].year}\``}
+                  className="experi-year-md"
+                />
+              </span>
+              {experi[page].title}
+            </h2>
+            <MarkdownPreview
+              source={experi[page].desc}
+              className="experience mt-3"
+            />
+          </div>
+          <FontAwesomeIcon
+            className="experi-page align-self-center"
+            icon={faAngleRight}
+            onClick={() => {
+              PageNext();
+            }}
+          ></FontAwesomeIcon>
         </div>
-        <FontAwesomeIcon
-          className="pe-2 experi-page align-self-center"
-          icon={faAngleRight}
-          onClick={() => {
-            PageNext();
-          }}
-        ></FontAwesomeIcon>
-      </div>
-      <div className="experi-page-status d-flex justify-content-center gap-2 mb-3">
+      </Container>
+      <div className="experi-page-status d-flex justify-content-center gap-2 mt-3 mb-3">
         {experi.map((_, i: number): JSX.Element => {
           return (
             <div
